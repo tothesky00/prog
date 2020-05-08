adc842da8d38d034adecc8728fd0577248602f44
tothesky00 <aya.konst.5398@gmail.com>
2020-05-08 18:21:46 +0900

    лабораторная работа 9 по вариантам

---
+++ MainClass.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("true or false");
+        boolean b = sc.nextBoolean();
+        System.out.println("You type = " + b);
+    }
+}
