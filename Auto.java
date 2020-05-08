1042ad3c875ce69b6197ac90c3594c509e3d0715
tothesky00 <aya.konst.5398@gmail.com>
2020-05-08 18:03:58 +0900

    пример 9

---
+++ Auto.java
@@ -0,0 +1,25 @@
+public class Auto {
+    private String firm; //создаем закрытый член нашего класса с названием фирмы автомобиля
+    private int maxSpeed; // закрытый член класса, содержащий максимальную скорость
+    public void setFirm(String firma){ //открытая функция (метод класса) для задания
+        firm=firma; //значения фирмы автомобиля
+    }
+    public void setMaxSpeed(int speed){ //открытая функция (метод класса) для задания
+        maxSpeed=speed; //значения максимальной скорости автомобиля
+    }
+    public int getMaxSpeed(){ //открытая функция (метод класса) для вывода значения
+        return maxSpeed; //максимальной скорости
+    }
+    public String getFirm(){ //открытая функция (метод класса) для вывода значения
+        return firm; //заданной фирмы
+    }
+    public Auto(){ // конструктор класса (без параметров)
+        firm="Без названия";
+        maxSpeed=0;
+    }
+    public Auto(String firma, int speed){ //конструктор класса (с параметрами)
+        firm=firma;
+        maxSpeed=speed;
+    }
+
+}
